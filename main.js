5b001b864897783f92f22fb1d1282a39193803d5
Luke Wilson <l2wilson94@gmail.com>
2021-02-06 09:35:35 +0000

    transpile

--- main.js
+++ main.js
@@ -34,8 +34,19 @@ const tick = () => {
 //===========//
 // Transpile //
 //===========//
+const stripComments = (source) => source.split("\n").map(line => line.split("//")[0]).filter(line => !line.is(WhiteSpace)).join("\n")
+
+const labelPositions = {}
+let currentPosition = 0
+
 MotherTode `
-Label :: LabelName ":"
+Line :: [_] Instruction [_] EOF >> ([_, i]) => i.output
+Instruction :: Label | Jump
+
+Label (
+	:: LabelName ":"
+	>> (label) => { labelPositions[label[0]] = currentPosition; return "// " + label[0]; }
+)
 LabelName :: /[a-z]/+
 
 Jump :: "Jmp" [_] LabelName >> ([j, _, l]) => "instructionPosition = getLabelPosition('" + l + "')"
@@ -45,16 +56,51 @@ IntLiteral :: /[1-9]/ (/[0-9]/+)?
 UIntLiteral :: IntLiteral "u"
 `
 
+
+
+const transpile = (source) => {
+	const strippedSource = stripComments(source)
+	const lines = strippedSource.split("\n")
+	const instructions = []
+	for (let i = 0; i < lines.length; i++) {
+		currentPosition = i
+		const line = lines[i]
+		const result = TERM.Line(line)
+		const instruction = result.output
+		instructions.push(instruction)
+	}
+	
+	return instructions
+}
+
+
+
+//==========//
+// Run-Time //
+//==========//
+const instructions = []
+const getLabelPosition = (name) => {
+	if (name === "exit") return instructions.length
+	return labelPositions[name]
+}
+
+let instructionPosition = 0
+
+//========//
+// Tinker //
+//========//
 const test = () => {
 	TERM.IntLiteral("5").d
 	TERM.UIntLiteral("5u").d
 	TERM.Label("loop:").d
 	TERM.Jump("Jmp loop").d
+
+	transpile(`
+		loop:
+		Jmp loop
+	`).d
+
+
 }
 
 test()
-
-//==========//
-// Run-Time //
-//==========//
-let instructionPosition = 0
